dd75936e99a5c4ce592d676ef6715fa3bda721ec
Lucas1806fera <melhordomundomessi7@gmail.com>
2021-02-19 18:13:24 -0300

    First commit

---
+++ src/zeusmenu.js
@@ -0,0 +1,40 @@
+const anamenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos da Aninha BOLADA:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}pornozin*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  TRADUZIDO POR *ZEUS*
+  DUVIDAS? 👇
+  WA.me/5511954580970 
+╚════════════════════`
+}
+
+exports.anamenu = zeusmenu
+
+
+
+
+
+
+
+
